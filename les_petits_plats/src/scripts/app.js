5ae9555fb8c80a81a6c5c48a43939bf5f5ee936b
inesder <inesderraz@hotmail.fr>
2024-05-01 12:19:43 +0200

    add dynamic html for cards

--- les_petits_plats/src/scripts/app.js
+++ les_petits_plats/src/scripts/app.js
@@ -6,22 +6,67 @@ function logRecipes() {
     const cardsSection = document.querySelector('.cards-section');
     recipes.forEach(recipe => {
         const recipeCard = document.createElement('div');
+        recipeCard.classList.add('card-element');
 
         const recipeImage = document.createElement('img');
-        recipeImage.setAttribute('src', '../assets/recipes_images/Recette40.jpg')
+        recipeImage.setAttribute('src', 'src/assets/recipes_images/' + recipe.image);
+        recipeImage.classList.add('recipe-img');
+
+        const recipeContainer = document.createElement('div');
+        recipeContainer.classList.add('recipe-container')
         const recipeName = document.createElement('h3');
         recipeName.textContent = recipe.name;
-        console.log(recipe.name);
+
+        const recipeLabel = document.createElement("p");
+        recipeLabel.textContent = "recette";
+        recipeLabel.classList.add('label');
+
+        const recipeDescription = document.createElement('p');
+        recipeDescription.textContent = recipe.description;
+        recipeDescription.classList.add('recipe-description');
+
+        const ingredientsContainer = document.createElement('div');
+        ingredientsContainer.classList.add('ingredients-container');
+
+        const ingredientLabel = document.createElement('p');
+        ingredientLabel.classList.add('label');
+        ingredientLabel.textContent = "Ingrédients"
+
         recipeCard.appendChild(recipeImage);
-        recipeCard.appendChild(recipeName);
+        recipeCard.appendChild(recipeContainer);
+        recipeContainer.appendChild(recipeName);
+        recipeContainer.appendChild(recipeLabel);
+        recipeContainer.appendChild(recipeDescription);
+        recipeContainer.appendChild(ingredientLabel);
         cardsSection.appendChild(recipeCard);
-        /*recipe.ingredients.forEach(ingredient => {
+
+        recipe.ingredients.forEach(ingredient => {
+
+            const ingredientInformations = document.createElement('div');
+
+            const recipeIngredients = document.createElement('p');
+            recipeIngredients.textContent = ingredient.ingredient;
+            recipeIngredients.classList.add('recipe-ingredients');
+
+            const ingredientQuantity = document.createElement('p');
+            ingredientQuantity.textContent = ingredient.quantity + ingredient.unit;
+            ingredientQuantity.classList.add('ingredient-quantity');
+
+            if (ingredient.quantity && ingredient.unit) {
+                ingredientQuantity.textContent = ingredient.quantity + ' ' + ingredient.unit;
+            } else if (ingredient.quantity) { // S'il n'y a pas d'unité mais une quantité
+                ingredientQuantity.textContent = ingredient.quantity;
+            } else { // Si ni quantité ni unité n'est fournie
+                ingredientQuantity.textContent = ''; // Ne rien mettre ou mettre un texte par défaut
+            }
+
+            ingredientInformations.appendChild(recipeIngredients);
+            ingredientInformations.appendChild(ingredientQuantity);
+            ingredientsContainer.appendChild(ingredientInformations);
+            recipeContainer.appendChild(ingredientsContainer);
 
         })
-        recipeName.textContent = recipes.name;
-        const recipeDescription = document.createElement("p");
-        const recipeIngredients = document.createElement('p');
-        */
+
     });
 }
 
