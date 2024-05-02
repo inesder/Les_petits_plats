479622380ad221ae1d31e82bb14de7823b40661b
inesder <inesderraz@hotmail.fr>
2024-05-02 14:57:44 +0200

    add filter section

--- les_petits_plats/src/scripts/app.js
+++ les_petits_plats/src/scripts/app.js
@@ -1,7 +1,8 @@
 // Dans app.js
 import { displayRecipes } from './cards.js';
-
+import { displayFilter } from './filter.js';
 
 // Appel de la fonction au chargement de la page
+document.addEventListener('DOMContentLoaded', displayFilter);
 document.addEventListener('DOMContentLoaded', displayRecipes);
 
